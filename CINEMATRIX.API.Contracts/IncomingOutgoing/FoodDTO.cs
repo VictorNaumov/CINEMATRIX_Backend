d9c9056b4f42a7e1cfce38031eb2554a282cc956
VictorNaumov <anakinsanakins@outlook.com>
2022-08-19 20:55:09 +0300

    cleanup

--- CINEMATRIX.API.Contracts/IncomingOutgoing/FoodDTO.cs
+++ CINEMATRIX.API.Contracts/IncomingOutgoing/FoodDTO.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace CINEMATRIX.API.Contracts.IncomingOutgoing
+﻿namespace CINEMATRIX.API.Contracts.IncomingOutgoing
 {
     public class FoodDTO
     {
--- CINEMATRIX.Data.EF.SQL/Migrations/20220819171500_InitDatabase.cs
+++ CINEMATRIX.Data.EF.SQL/Migrations/20220819171500_InitDatabase.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using Microsoft.EntityFrameworkCore.Migrations;
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
 
 namespace CINEMATRIX.Data.EF.SQL.Migrations
 {
