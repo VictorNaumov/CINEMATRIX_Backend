cc1597c4e28abcae5f14a34639c1f01c02c73fde
VictorNaumov <anakinsanakins@outlook.com>
2022-08-24 20:00:52 +0300

    add migration

---
+++ CINEMATRIX.Data.EF.SQL/Migrations/20220824165819_RenamePersonalDataToProfile.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CINEMATRIX.Data.EF.SQL.Migrations
+{
+    public partial class RenamePersonalDataToProfile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_PersonalData_PersonalDataId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "PersonalData");
+
+            migrationBuilder.RenameColumn(
+                name: "PersonalDataId",
+                table: "Users",
+                newName: "ProfileId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_PersonalDataId",
+                table: "Users",
+                newName: "IX_Users_ProfileId");
+
+            migrationBuilder.CreateTable(
+                name: "Profile",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecondName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Profile", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Profile_ProfileId",
+                table: "Users",
+                column: "ProfileId",
+                principalTable: "Profile",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Profile_ProfileId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Profile");
+
+            migrationBuilder.RenameColumn(
+                name: "ProfileId",
+                table: "Users",
+                newName: "PersonalDataId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Users_ProfileId",
+                table: "Users",
+                newName: "IX_Users_PersonalDataId");
+
+            migrationBuilder.CreateTable(
+                name: "PersonalData",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DateOfBirth = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecondName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalData", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_PersonalData_PersonalDataId",
+                table: "Users",
+                column: "PersonalDataId",
+                principalTable: "PersonalData",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
